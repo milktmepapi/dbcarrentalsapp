c696820309df892648aa380589df2f681865ffba
Lance <lance_galicia@dlsu.edu.ph>
2025-11-11 22:29:00 +0800

    Initial commit

---
+++ dummy/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.dummy {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.dummy to javafx.fxml;
+    exports com.example.dummy;
+}
